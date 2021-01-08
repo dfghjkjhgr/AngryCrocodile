7948f14117179f8f4d8132a6da384bc9288a0230
dfghjkjhgr <67027632+dfghjkjhgr@users.noreply.github.com>
2021-01-08 15:34:10 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -13,12 +13,29 @@ input.onGesture(Gesture.LogoDown, function () {
 let man: game.LedSprite = null
 man = game.createSprite(0, 2)
 let croc1 = game.createSprite(1, 2)
+let croc2 = game.createSprite(2, 2)
+let croc3 = game.createSprite(3, 2)
+let score = 0
+let h = game.createSprite(4, 0)
+let o = game.createSprite(4, 1)
 basic.forever(function () {
     if (man.isTouching(croc1)) {
         game.gameOver()
     }
+    if (man.isTouching(croc2)) {
+        game.gameOver()
+    }
+    if (man.isTouching(croc3)) {
+        game.gameOver()
+    }
+    if (man.isTouchingEdge()) {
+        score += 1
+        man.set(LedSpriteProperty.X, 0)
+    }
 })
 basic.forever(function () {
     croc1.set(LedSpriteProperty.Y, randint(0, 5))
-    basic.pause(200)
+    basic.pause(5000)
+    croc2.set(LedSpriteProperty.Y, randint(0, 5))
+    croc3.set(LedSpriteProperty.Y, randint(0, 5))
 })
