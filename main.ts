1798f95264faf53065728506ae236ecfff83d968
dfghjkjhgr <67027632+dfghjkjhgr@users.noreply.github.com>
2021-01-08 10:04:41 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    man.move(-1)
+})
 input.onGesture(Gesture.LogoUp, function () {
     man.change(LedSpriteProperty.Y, 1)
 })
-input.onGesture(Gesture.TiltLeft, function () {
-    man.move(-1)
-})
-input.onGesture(Gesture.TiltRight, function () {
+input.onButtonPressed(Button.B, function () {
     man.move(1)
 })
 input.onGesture(Gesture.LogoDown, function () {
@@ -12,6 +12,13 @@ input.onGesture(Gesture.LogoDown, function () {
 })
 let man: game.LedSprite = null
 man = game.createSprite(0, 2)
+let croc1 = game.createSprite(1, 2)
 basic.forever(function () {
-	
+    if (man.isTouching(croc1)) {
+        game.gameOver()
+    }
+})
+basic.forever(function () {
+    croc1.set(LedSpriteProperty.Y, randint(0, 5))
+    basic.pause(200)
 })
